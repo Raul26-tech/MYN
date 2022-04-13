b2f57022a653aceb0f0276a7ad8ab96ca5a62ab2
Raul26-tech <rauletec2020@gmail.com>
2022-04-13 11:10:40 -0300

    Adicionando o backend

--- banco/db.sql
+++ banco/db.sql
@@ -16,17 +16,17 @@ CREATE TABLE tbl_usuario(
     tipo_usuario VARCHAR(13) NOT NULL
 );
 DESCRIBE tbl_usuario;
-
 ALTER TABLE tbl_usuario ADD usuarios_desativados VARCHAR(15);
 
 # TABLE PERFIL
+
 CREATE TABLE tbl_perfil(
 	id_perfil INT PRIMARY KEY AUTO_INCREMENT,
     perfil_user INT,
     FOREIGN KEY (perfil_user) REFERENCES tbl_usuario(id_usuario)
 );
 
-
+# TABLE POSTS
 CREATE TABLE tbl_postUser(
 	id_post INT PRIMARY KEY AUTO_INCREMENT,
     title_post VARCHAR(150) NOT NULL,
@@ -36,3 +36,14 @@ CREATE TABLE tbl_postUser(
 );
 
 SELECT * FROM tbl_postUser;
+
+# TABLE SERVIÇOS
+CREATE TABLE servicos(
+	id_servico INT PRIMARY KEY AUTO_INCREMENT,
+    tipo_servico VARCHAR(45) NOT NULL,
+    descricao_seervico VARCHAR(100),
+    fk_servico INT,
+    FOREIGN KEY (fk_servico) REFERENCES tbl_perfil(id_perfil)
+);
+ALTER TABLE servicos RENAME TO tbl_servicos;
+DESCRIBE servicos;
